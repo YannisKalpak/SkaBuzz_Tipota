713da8cb5c95297cf695a95e8d7055c45b2673d9
DimitrisE <50424266+DimitrisE@users.noreply.github.com>
2019-06-01 16:16:25 +0300

    Update ClassicMode.java

--- ClassicMode.java
+++ ClassicMode.java
@@ -1,30 +1,46 @@
-import java.util.Scanner;
+import java.util.*;
+
 
 public class ClassicMode extends Modes{
-	private int CurrentQuestion;
 	
-	public ClassicMode(DataBase QuestionsData,String CurrentCategory) {
+	
+	public ClassicMode(DataBase QuestionsData,String CurrentCategory,int CurrentCategoryPos,Players[] AllPlayers) {
 		ModeName = "Classic Mode";
 		Scanner input = new Scanner(System.in);
 		
-		QuestionsData.CompleteStack( Integer.parseInt(CurrentCategory) );
-		
-		int CurrentQuestion = QuestionsData.PickingStack.peek();
-		
-		System.out.println(QuestionsData.Questions[CurrentQuestion]);
-		System.out.println(QuestionsData.Answers[CurrentQuestion][0] + " " + QuestionsData.Answers[CurrentQuestion][1]);
-		System.out.println(QuestionsData.Answers[CurrentQuestion][2] + " " + QuestionsData.Answers[CurrentQuestion][3]);
-
-		int PlayersAnswer = input.nextInt() - 1;
-		
-		if(QuestionsData.CheckAnswer(PlayersAnswer) == true) {
-			System.out.println("Well done");
-		}
-		else {
-			System.out.println("You suck noob");
-			System.out.println("The correct answer was : " + QuestionsData.Answers[CurrentQuestion][QuestionsData.CorrectAnswers[CurrentQuestion]]);
-			//mporoume na kanoyme kai mia sinartisi ReturnCorrectAnswer gia na min grafoume olo afto to palamari 
+		ArrayList<Players> PlayersList = new ArrayList<Players>();
+		for(int i=0;i<4;i++) {
+			PlayersList.add( AllPlayers[i]);
 		}
 		
+		
+		QuestionsData.CompleteStack( CurrentCategoryPos );
+		
+		
+		for(int i=0;i<8;i++) {
+			for(Players CurrentPlayer : PlayersList) 
+			{
+				
+				System.out.println("Σειρά του χρήστη " + CurrentPlayer.getPlayerName() + " για να παίξει");
+				
+				int CurrentQuestion = QuestionsData.PickingStack.peek();
+			
+				System.out.println(QuestionsData.Questions[CurrentQuestion]);            // ta minimata afta tha emfanizontai sto GUI
+				System.out.println(QuestionsData.Answers[CurrentQuestion][0] + " " + QuestionsData.Answers[CurrentQuestion][1]);
+				System.out.println(QuestionsData.Answers[CurrentQuestion][2] + " " + QuestionsData.Answers[CurrentQuestion][3]);
+	
+				int PlayersAnswer = input.nextInt() - 1;  //edw tha epistrefete i timi analoga me to ti exei pathsei o xristis 
+			
+				if(QuestionsData.CheckAnswer(PlayersAnswer,CurrentQuestion) == true) {
+					System.out.println("Σωραίος");
+					CurrentPlayer.addPoints(100);
+				}
+				else {
+					System.out.println("Είσαι γτπ");
+					QuestionsData.ShowCorrectAnswer(CurrentQuestion);
+				}	
+			} 
+			QuestionsData.PickingStack.pop();
+		}	
 	}
 }
